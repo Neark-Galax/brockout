f3d047323780348bf17ae77f5937b54902ad119f
kumakurakosuke <s2023ee27@oyama.kosen-ac.jp>
2023-07-09 16:42:12 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,105 @@
-//aiueo
+const MainCanvas = document.getElementById("MainCanvas");
+const MainContext = MainCanvas.getContext("2d");
+const CanvasWrapper = document.querySelector("#wrapper");
+const GameArea = new CanvasManager(new Vector2(1280, 720), MainCanvas, CanvasWrapper);
+const keyInput = new keyInputManager();
+const Sound = new SoundManager();
+GameArea.refresh();
+
+let IsGameRunning = false;
+
+//動かすバー
+
+const bar = new CanvasComponents({
+    ctx: MainContext,
+    img: "assets/bar.png",
+    size: new Vector2(124, 15),
+    position: new Vector2(GameArea.x / 2, GameArea.y - 100),
+    update: function () {
+        if (keyInput.key["a"] && this.position.x > 0 + this.size.x / 2) {
+            this.position.x -= 10;
+        }
+        if (keyInput.key["d"] && this.position.x < GameArea.x - this.size.x / 2) {
+            this.position.x += 10;
+        }
+    },
+});
+const ball = new CanvasComponents({
+    ctx: MainContext,
+    img: "assets/ball.png",
+    size: new Vector2(36, 36),
+    position: new Vector2(GameArea.x / 2, GameArea.y / 2),
+    update: function () {
+        if (IsGameRunning) {
+            if (this.position.x < 0 + this.size.x / 2) {
+                this.direction.x = Math.abs(this.direction.x);
+                Sound.PlaySound("hit");
+            } else if (this.position.x > GameArea.x - this.size.x / 2) {
+                this.direction.x = Math.abs(this.direction.x) * -1;
+                Sound.PlaySound("hit");
+            }
+            if (this.position.y < 0 + this.size.y / 2) {
+                this.direction.y = Math.abs(this.direction.y);
+                Sound.PlaySound("hit");
+            }
+            if (
+                this.position.y > bar.position.y - bar.size.y / 2 - this.size.y / 2 &&
+                this.position.y < bar.position.y + bar.size.y / 2 + this.size.y / 2 &&
+                this.position.x > bar.position.x - bar.size.x / 2 - this.size.x / 2 &&
+                this.position.x < bar.position.x + bar.size.x / 2 + this.size.x / 2
+            ) {
+                let hitPosition = (this.position.x - bar.position.x) / (bar.size.x / 2);
+                this.direction = new Vector2(2 * hitPosition, -1);
+                Sound.PlaySound("hit");
+            }
+            if (this.position.y > GameArea.y - this.size.y / 2) {
+                gameOver();
+            }
+            this.motion = this.direction.normalized().multiply(15);
+            this.position = this.position.add(this.motion);
+        }
+    },
+});
+ball.direction = new Vector2(0, 0);
+
+Sound.LoadSound("click", "assets/click.mp3");
+Sound.LoadSound("hit", "assets/hit.mp3");
+function gameStart() {
+    Sound.PlaySound("click");
+    document.querySelector("#menu").style.display = "none";
+    document.querySelector("#game").style.display = "block";
+    bar.position = new Vector2(GameArea.x / 2, GameArea.y - 100);
+    ball.position = new Vector2(GameArea.x / 2, GameArea.y / 2);
+    ball.direction = new Vector2(Math.random() * 0.5 - 0.25, 1);
+    IsGameRunning = true;
 
 
+function gameOver() {
+    document.querySelector("#gameEnd").style.display = "block";
+    IsGameRunning = false;
+}
 
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-var ballRadius = 10;
-var x = canvas.width/2;
-var y = canvas.height-30;
-var dx = 2;
-var dy = -2;
-var paddleHeight = 10;
-var paddleWidth = 75;
-var paddleX = (canvas.width-paddleWidth)/2;
-var rightPressed = false;
-var leftPressed = false;
-var brickRowCount = 3;
-var brickColumnCount = 5;
-var brickWidth = 75;
-var brickHeight = 20;
-var brickPadding = 10;
-var brickOffsetTop = 30;
-var brickOffsetLeft = 30;
+function backMenu() {
+    Sound.PlaySound("click");
+    document.querySelector("#menu").style.display = "block";
+    document.querySelector("#game").style.display = "none";
+    document.querySelector("#gameEnd").style.display = "none";
+}
+
+function update() {
+    
+}
+
+//ゲームループの定義・開始
+const GameLoop = new GameLoopManager(() => {
+    MainContext.clearRect(0, 0, GameArea.x, GameArea.y);
+    CanvasComponents.components.forEach((component) => {
+        component.update();
+        component.render();
+    });
+    update();
+}, 30);
+GameLoop.start();
 
 var bricks = [];
 for(var c=0; c<brickColumnCount; c++) {
@@ -42,6 +121,14 @@ function keyDownHandler(e) {
     }
 }
 
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
 function keyUpHandler(e) {
     if(e.key == "Right" || e.key == "ArrowRight") {
         rightPressed = false;
@@ -58,13 +145,7 @@ function drawBall() {
     ctx.fill();
     ctx.closePath();
 }
-function drawPaddle() {
-    ctx.beginPath();
-    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
-}
+
 function drawBricks() {
     for(var c=0; c<brickColumnCount; c++) {
         for(var r=0; r<brickRowCount; r++) {
